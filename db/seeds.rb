6e5b7c10905c0947e313b822dc97f4a095ee5ff7
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2020-03-30 14:50:37 -0400

    add validations for phonenumber to ensure proper length and format. modify partysize form to be a select

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,10 @@ restaurant.save
 # Seed reservations for demo (* Remove for production *)
 namesArray = [ "Hamed M", "Erin D", "Ray M", "Kevin L", "Connor D", 'Maureen H', "Karen L", "Bob L", "Meaghan B", "Luke B", "Paxton B", "Isla B" ]
 phonenumbersArray = [905, 416, 705, 647, 519]
-testPhoneNum = "#{phonenumbersArray[rand(5)]}-#{rand(100...1000)}-#{rand(1000...10000)}"
 timeArray = [1700, 1800, 1900, 2000, 2100]
+notesArray = ["VIP", "Would like a table by the window", "Celebrating a birthday"]
 dateArray = []
+
 
 # create 150 days in the future
 x = 0
@@ -31,16 +32,17 @@ x = 0
   x += 1
 end
 
-dateArray.length.times do
-  dateIndex = 0
+dateIndex = 0
+74.times do
   3.times do
     reservation = Reservation.new
     reservation.name = namesArray[rand(0...namesArray.length)]
     reservation.phonenumber = "#{phonenumbersArray[rand(5)]}-#{rand(100...1000)}-#{rand(1000...10000)}"
-    reservation.partysize = rand(0...6)
+    reservation.partysize = rand(1..10)
     reservation.date = dateArray[dateIndex]
     reservation.time = timeArray[rand(0...timeArray.length)]
+    reservation.notes = notesArray[rand(0...notesArray.length)]
     reservation.save
   end
-  dateIndex += 1
+  dateIndex += 2
 end
